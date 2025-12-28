fafcfef788b08bf6908df5681cac9d09aa7ed3e6
Ajay Yadav <ajay@example.com>
2025-12-28 11:00:34 +0530

    fix(auth): correct middleware parameter order in JWT protection

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,34 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
+export const protect = async (req, res, next) => { 
+  let token;
 
-export const protect = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    return res.status(401).json({ message: "Unauthorizated" });
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
   }
 
-  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Not authorized, token missing",
+    });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    req.user = await User.findById(decoded.id).select("-password");
+
     next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Not authorized, invalid token",
+    });
   }
 };
